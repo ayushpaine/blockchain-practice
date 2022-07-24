76107da7264371316e90c7c14c22d49d477659aa
ayushpaine <ayushmt701@gmail.com>
2022-07-24 09:11:01 +0530

    using truffle as a dev env

---
+++ contracts/Faucet.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+    uint256 public funds = 1000;
+    int256 public counter = -10;
+    uint256 public test = 0;
+}
