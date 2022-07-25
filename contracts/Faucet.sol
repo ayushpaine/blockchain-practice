09c678b344058719d77586107278794dc2476c23
ayushpaine <ayushmt701@gmail.com>
2022-07-25 15:28:31 +0530

    external payable pure view json-rpc

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -5,6 +5,16 @@ contract Faucet {
     //special function called when you make a transaction that doesnt specify function name to call
     //ext func part of contract interface which means they can be called  via contracts and other transactions
     receive() external payable {}
+
     //nonce - a hash that proves that the blockhas gone through pow
     //8 bytes
+    function addFunds() external payable {}
+
+    function testing() external pure returns (uint256) {
+        return 2 + 2;
+    }
+
+    //view indicates that the fn will not alter the storage state in any way
+    //pure indicates that the fn will not even real the storage state - stricter
+    //pure, view - read only (no gas fee)
 }
