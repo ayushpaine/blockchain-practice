c7e12985f864e6d3db6a9366c0467d5b056d336d
ayushpaine <ayushmt701@gmail.com>
2022-07-28 11:42:47 +0530

    storage stuff

---
+++ contracts/Storage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Storage {
+    uint8 public a = 7;
+    uint16 public b = 10;
+    address public c = 0x7C98675d67Bbd233619226A55C64B6A6eC934793;
+    bool d = true;
+    uint64 public e = 15;
+    uint256 public f = 200;
+    uint8 public g = 40;
+    uint256 public h = 789;
+}
