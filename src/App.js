31fee2f6f503f60899b1498821ec1d6b621b9ac3
ayushpaine <ayushmt701@gmail.com>
2022-07-30 22:33:54 +0530

    styling improved and check added when user is not signed in to metamask

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 //metamask injects a global api into websites
 //this api allows websites to request users, accoutns, read data to blockchain
@@ -7,31 +9,67 @@ import { useEffect } from "react";
 //with metamask we have an access to window.web3 and window.ethereum
 
 function App() {
+  const [web3API, setWeb3API] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  const [account, setAccount] = useState(null);
+
   useEffect(() => {
-    const loadProvider = async () => {};
+    const loadProvider = async () => {
+      let provider = await detectEthereumProvider();
+
+      if (provider) {
+        setWeb3API({
+          web3: new Web3(provider),
+          provider,
+        });
+      } else {
+        console.log("Insall Metamask");
+      }
+    };
 
     loadProvider();
   }, []);
 
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3API.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+
+    web3API.web3 && getAccount();
+  }, [web3API.web3]);
+
+  console.log(account);
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <div className="balance-view is-size-2">
+          <span>
+            <strong className="mr-2">Account: </strong>
+          </span>
+          <span>
+            {account ? (
+              account
+            ) : (
+              <button
+                className="button is-small"
+                onClick={() =>
+                  web3API.provider.request({ method: "eth_requestAccounts" })
+                }
+              >
+                Connect Wallet
+              </button>
+            )}
+          </span>
+          <div className="balance-view is-size-2 my-2">
             Current Balance: <strong>10</strong> ETH
           </div>
-          <div
-            className="button-wrapper"
-            onClick={async () => {
-              const accounts = await window.ethereum.request({
-                method: "eth_requestAccounts",
-              });
-              console.log(accounts);
-            }}
-          >
-            <button className="btn mr-2">Enable Ethereum</button>
-            <button className="btn mr-2">Donate</button>
-            <button className="btn">Withdraw</button>
+          <div className="button-wrapper">
+            <button className="button is-primary mr-2">Donate</button>
+            <button className="button is-link ">Withdraw</button>
           </div>
         </div>
       </div>
